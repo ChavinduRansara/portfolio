cbd8145ba7572aa20c52cd09af1878e19adebefe
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2025-01-03 21:21:36 +0530

    fix: update Angular asset import to match casing

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import java from '../assets/Java.svg';
-import angular from '../assets/angular.svg';
+import angular from '../assets/Angular.svg';
 import cpp from '../assets/cpp.svg';
 import express from '../assets/Express.svg';
 import flutter from '../assets/Flutter.svg';
